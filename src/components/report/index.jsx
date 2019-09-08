bb78a3a5c73aa7c0482a9349228fdf89fa0e4997
SMC-Melvin <mreston@smchindang.edu.ph>
2019-09-08 15:12:02 +0800

    Reports

--- src/components/report/index.jsx
+++ src/components/report/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './style.scss';
 
 import 'jquery';
 import 'popper.js';
@@ -14,10 +15,148 @@ class Report extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="container">
         <header>
-          <img src={logo} alt="" />
+          <div className="text-center">
+            <img
+              src={logo}
+              alt=""
+              width="50px"
+              height="50px"
+              className="margin-50"
+            />
+            <h2 className="text-uppercase">
+              Saint Micheal College of Hindang, Leyte, Inc.
+            </h2>
+            <h5>A. Bonifacio St., Poblacion II, Hindang, Leyte</h5>
+          </div>
         </header>
+        <table width="100%" className="margin-top-50 table" border="1">
+          <thead>
+            <tr className="text-center">
+              <th>Can#</th>
+              <th>Name</th>
+              <th>Faction</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="text-center">
+              <td>1</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>2</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>3</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>4</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>5</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>6</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>6</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>7</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>1</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>2</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>3</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>4</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>5</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>6</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>6</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+            <tr className="text-center">
+              <td>7</td>
+              <td>Pilyang Husband</td>
+              <td>A</td>
+              <td>10</td>
+            </tr>
+          </tbody>
+        </table>
+        <div>
+          <div className="col-md-12 judges">
+            <div className="row text-center">
+              <div className="col-md-4">
+                <h3>Judge Name</h3>
+                <h5>Judge 1</h5>
+              </div>
+              <div className="col-md-4">
+                <h3>Judge Name</h3>
+                <h5>Judge 2</h5>
+              </div>
+              <div className="col-md-4">
+                <h3>Judge Name</h3>
+                <h5>Judge 3</h5>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
